8aac6897b08147099b7de3cb03f16f89535c2199
Lucas Holanda <luks206@gmail.com>
2022-08-07 21:27:05 -0300

    Organizando a Home

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php include_once("templates/header.php"); ?>
 
-<h1>Primeiro Post</h1>
+<main>
+    <!--título da pagina-->
+    <div id="title-container">
+        <h1>Blog Codar</h1>
+        <p>Um Blog de Programação</p>
+    </div>
+
+    <!--Container para todos os posts-->
+    <div id="posts-container">
+        <?php foreach($posts as $post):?>
+            
+            <div class="post-box"> <!-- container para exibir a informação do post-->
+                <img src="img/<?= $post['img']?>" alt="<?= $post['title']?>">
+                
+                <h2 class="post-title">
+                    <a href="/post.php?id=<?= $post['id']?>"> <?= $post['title']?> </a>
+                </h2>
+
+                <p class="post-description"> <?= $post['description']?> </p>
+                
+                <div class="tags-container">
+                    <?php foreach($post['tags'] as $tag):?>
+                        <a href="#"> <?= $tag ?> </a>
+                    <?php endforeach;?>
+                </div>
+            </div>
+
+        <?php endforeach;?>    
+    </div>
+
+</main>
     
 <?php include_once("templates/footer.php"); ?>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,8 @@
+<?php
+    include_once("data/categories.php");
+    include_once("data/posts.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
